3c1a115960cd5ee9be9a7468567302be6c2e8b05
langzilang <1874815111@qq.com>
2022-04-27 17:04:55 +0800

    :seedling: update files

---
+++ src/02-组合API的生命周期钩子使用.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+<!--    根组件-->
+    <h1 @click="say">{{ msg }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name:'App',
+  // 1. 组合API的起点，将来的组合API的代码，基本上在这里
+  // 2. 可以理解为：在beforeCreate钩子函数前执行，组件实例创建前
+  // 3. 函数中不能使用this  ==> undefined
+  // 4. 末班中需要使用的数据和函数，需要在setup中返回  ==> 因此返回的应该是一个对象，而不是一个值
+  setup () {
+    console.log('setup',this)
+    // 数据
+    const msg = 'hi vue3'
+    // 函数
+    const say = () => {
+      console.log('hi,vue3')
+    }
+
+    return { msg, say }
+  },
+  beforeCreate () {
+    console.log('beforeCreate',this)
+  }
+}
+</script>
--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,28 @@
 <template>
   <div class="container">
 <!--    根组件-->
-    <h1 @click="say">{{ msg }}</h1>
+    <div @click="updateName">
+      <div>{{  obj.name }}</div>
+      <div>{{ obj.age }}</div>
+    </div>
   </div>
 </template>
 <script>
+import { reactive } from "vue";
+
 export default {
   name:'App',
-  // 1. 组合API的起点，将来的组合API的代码，基本上在这里
-  // 2. 可以理解为：在beforeCreate钩子函数前执行，组件实例创建前
-  // 3. 函数中不能使用this  ==> undefined
-  // 4. 末班中需要使用的数据和函数，需要在setup中返回  ==> 因此返回的应该是一个对象，而不是一个值
   setup () {
-    console.log('setup',this)
-    // 数据
-    const msg = 'hi vue3'
-    // 函数
-    const say = () => {
-      console.log('hi,vue3')
+    const obj = reactive({
+      name:'zebras',
+      age:18
+    })
+    
+    const updateName = () => {
+      console.log( 'updateName', updateName )
+      obj.name = 'zebra'
     }
-
-    return { msg, say }
-  },
-  beforeCreate () {
-    console.log('beforeCreate',this)
+    return { obj, updateName }
   }
 }
 </script>
