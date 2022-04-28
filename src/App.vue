2d53dfe9418a1b3da382940a695c115b61daced6
langzilang <1874815111@qq.com>
2022-04-28 16:41:32 +0800

    :monocle_face:  update files

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
   <div class="container">
 <!--    根组件-->
     <div @click="updateName">
-      <div>{{  name }}</div>
-      <div>{{ age }}</div>
+<!--      <div>{{  name }}</div>-->
+<!--      <div>{{ age }}</div>-->
     </div>
   </div>
 </template>
 <script>
-import { reactive, toRef } from "vue";
+import { reactive } from "vue";
 
 export default {
   name:'App',
@@ -17,19 +17,26 @@ export default {
       name:'zebras',
       age:18
     })
-    console.log( 'obj', obj )
+    console.log( 'obj', obj )  //此时obj还是响应式数据对象， Proxy {name: 'zebras', age: 18}
     // ⚠注意：从响应式数据对象中解构出来的属性数据，不再是响应式数据
     // let { name, age } = obj
     
-    let name = toRef(obj, 'name')
-    let age = toRef(obj, 'age')
+    // console.log( 'name', name )
+    // console.log( 'age', age )
+    
+    const { name, age } = obj
     console.log( 'name', name )
+    console.log( 'age', age )
+    const obj2 = { ...obj }
+    console.log( 'obj2', obj2 )  //此时obj2 就成了普通的对象数据， {name: 'zebras', age: 18}
     const updateName = () => {
       console.log( 'updateName', updateName )
       // toRef转换响应式数据包装成对象，value是存放值的地方
       name.value = 'zebra'
     }
-    return { name, age, updateName }
+    // 以上方式会导致数据不是响应式数据
+    return { }
+    // return { name, age, updateName }
   }
 }
 </script>
